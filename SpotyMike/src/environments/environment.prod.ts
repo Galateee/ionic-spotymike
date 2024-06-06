8891e1671fe286e9930639fc2683466e1d806944
Robin Vidal <robinvidal01@gmail.com>
2024-06-06 20:42:09 +0200

    firebase swap

--- SpotyMike/src/environments/environment.prod.ts
+++ SpotyMike/src/environments/environment.prod.ts
@@ -2,11 +2,12 @@ export const environment = {
   production: true,
   url_api: 'http://esiea-spotymike.eu-4.evennode.com/v1',
   firebase: {
-    apiKey: "AIzaSyCJlU24TnJt-WKc6pNt1qiUi_uE9bx3bZA",
-    authDomain: "esiea-truc-747b2.firebaseapp.com",
-    projectId: "esiea-truc-747b2",
-    storageBucket: "esiea-truc-747b2.appspot.com",
-    messagingSenderId: "123609914325",
-    appId: "1:123609914325:web:f4ad82e5a5f4ba39eabb5c"
+    apiKey: "AIzaSyDdqpya4cEPHo4YXE3q8OhyhUJ882O6H9w",
+    authDomain: "esiea-spotymikea.firebaseapp.com",
+    projectId: "esiea-spotymikea",
+    storageBucket: "esiea-spotymikea.appspot.com",
+    messagingSenderId: "301165076577",
+    appId: "1:301165076577:web:d62cde051ef467d27aca20",
+    measurementId: "G-3GCMD6B8KX"
   },
 };
